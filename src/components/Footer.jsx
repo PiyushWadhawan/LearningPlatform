3ad1600abbaa42185dc92cdafd4f3d2628c0713b
PIYUSH WADHAWAN <wa.piyush@gmail.com>
2023-01-06 02:03:29 +0530

    Created navbar and intro content

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <p>2022 Copyright © <span>dorabhai</span></p>
+    </div>
+  )
+}
+
+export default Footer
