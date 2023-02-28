37e3f652061c51476568bef08dff042cf1b05057
PIYUSH WADHAWAN <wa.piyush@gmail.com>
2023-02-28 15:00:58 +0530

    Updated Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css"
 function Footer() {
   return (
     <div className='footer'>
-      <p>2022 Copyright © <span>E-Learn</span></p>
+      <p>2023 Copyright © <span>E-Learn</span></p>
     </div>
   )
 }
